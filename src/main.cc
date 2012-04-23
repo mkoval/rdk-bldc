d75b747cf41a6f8cb51d18ae67e87fb9d301c7a0
Michael Koval <koval.michael@gmail.com>
2012-04-23 15:08:59 -0400

    Switched to ros::Rate for timing.

--- src/main.cc
+++ src/main.cc
@@ -1,26 +1,34 @@
 #include <boost/date_time/posix_time/posix_time.hpp>
 #include <boost/make_shared.hpp>
 #include <boost/thread.hpp>
+#include <ros/ros.h>
 #include "rdk-bldc.hh"
 #include "robot.hh"
 
 typedef boost::shared_ptr<MotorController> MotorControllerPtr;
 
-static std::string const kIPLeft  = "192.168.1.101";
-static std::string const kIPRight = "192.168.1.102";
-static std::string const kPort    = "23";
-static long const kPeriodMs = 50;
+static ros::Subscriber sub_twist;
+static std::string ip_left, ip_right, port;
+static double rate_hz;
 
 int main(int argc, char **argv)
 {
-    MotorControllerPtr driver_left  = boost::make_shared<MotorController>(kIPLeft, kPort);
-    MotorControllerPtr driver_right = boost::make_shared<MotorController>(kIPLeft, kPort);
+    ros::init(argc, argv, "dar_driver_node");
+    ros::NodeHandle nh, nh_priv("~");
+
+    nh_priv.param<std::string>("ip_left", ip_left, "192.168.1.101");
+    nh_priv.param<std::string>("ip_right", ip_right, "192.168.1.102");
+    nh_priv.param<std::string>("port", port, "23");
+    nh_priv.param<double>("rate_hz", rate_hz, 20);
+
+    MotorControllerPtr driver_left  = boost::make_shared<MotorController>(ip_left, port);
+    MotorControllerPtr driver_right = boost::make_shared<MotorController>(ip_right, port);
     Robot robot(driver_left, driver_right);
 
-    for (;;) {
-        robot.spin(kPeriodMs);
-        boost::this_thread::sleep(boost::posix_time::milliseconds(kPeriodMs)); 
+    ros::Rate rate(rate_hz);
+    while (ros::ok()) {
+        robot.spin(1 / rate_hz);
+        rate.sleep();
     }
-
     return 0;
 }
